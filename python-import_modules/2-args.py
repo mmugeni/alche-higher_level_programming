156abcb2eaf31112e74114d2003ddf39c1f84553
Moulaika <m.mugeni@alustudent.com>
2025-08-17 23:07:13 -0700

    Scripts

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv[1:]  # list of arguments without the script name
+    count = len(args)
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))

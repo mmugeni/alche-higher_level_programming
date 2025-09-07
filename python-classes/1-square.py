fbffc2c6e62a84ea0086d6d440a0e228f7a643e9
Moulaika <m.mugeni@alustudent.com>
2025-09-07 09:57:36 -0700

    shell script

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Defines a class Square with a private size attribute."""
+
+
 class Square:
+    """Represents a square with a private size."""
+
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
